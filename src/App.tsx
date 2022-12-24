03aab443f78d5cf370cb4c9d6101e2e7d0ac776c
jhyoon <jhyoon@rsupport.com>
2022-12-24 16:19:50 +0900

    style: 눈 내리는 배경

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import styled from "styled-components";
+import { Reset } from "styled-reset";
+import Snowfall from "react-snowfall";
+
 function App() {
-	return <div id="app"></div>;
+	return (
+		<StyledApp id="app">
+			<Reset />
+			<Snowfall />
+		</StyledApp>
+	);
 }
 
 export default App;
+
+const StyledApp = styled.div`
+	position: fixed;
+	width: 100%;
+	height: 100%;
+	background-color: #333;
+`;
